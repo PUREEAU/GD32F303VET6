45edfce797af8d58bfb98cfbd5a48c290fae5180
PUREEAU <pureeau@qq.com>
2026-04-15 22:14:52 +0800

    up data by 26.4.5

--- User/main.c
+++ User/main.c
@@ -35,6 +35,11 @@ int main(void)
     lv_port_disp_init();
     lv_port_indev_init();
 
+    lv_obj_t * scr = lv_disp_get_scr_act(NULL);
+    lv_obj_t * label = lv_label_create(scr);
+    lv_label_set_text(label, "Hello, World!");
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    
     while (1)
     {
 
